aea1f07bcc54a034d0d22b115d0c72bd70321d1e
gzhao9 <74684732+gzhao9@users.noreply.github.com>
2022-10-10 22:11:03 -0400

    skip error file and get RQ 4

--- src/astsimple/handlers/GetInfo.java
+++ src/astsimple/handlers/GetInfo.java
@@ -13,10 +13,10 @@ import org.eclipse.core.resources.IWorkspaceRoot;
 import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.jdt.core.ICompilationUnit;
+import org.eclipse.jdt.core.IImportDeclaration;
 import org.eclipse.jdt.core.IPackageFragment;
 import org.eclipse.jdt.core.IPackageFragmentRoot;
 import org.eclipse.jdt.core.JavaCore;
-import org.eclipse.jdt.core.JavaModelException;
 import org.eclipse.jdt.core.dom.AST;
 import org.eclipse.jdt.core.dom.ASTParser;
 import org.eclipse.jdt.core.dom.CompilationUnit;
@@ -24,125 +24,128 @@ import org.eclipse.jdt.core.dom.MethodInvocation;
 
 public class GetInfo extends AbstractHandler {
 	private static final String JDT_NATURE = "org.eclipse.jdt.core.javanature";
-	private ArrayList<String> mockito_arr = new ArrayList<>();
-	private ArrayList<String> easymock_arr = new ArrayList<>();
-
-	private ArrayList<String> err_arr = new ArrayList<>();
 
 	@Override
 	public Object execute(ExecutionEvent event) throws ExecutionException {
 
 		IWorkspace workspace = ResourcesPlugin.getWorkspace();
 		IWorkspaceRoot root = workspace.getRoot();
-		mockito_arr.clear();
-		easymock_arr.clear();
-		err_arr.clear();
 		IProject[] projects = root.getProjects();
-		System.out.println(projects.length);
-		// Loop over all projects
-		for (IProject project : projects) {
-			try {
-				if (project.isNatureEnabled(JDT_NATURE)) {
-					analyseMethods(project);
-				}
-			} catch (CoreException e) {
-				System.out.println("error in " + project.getName());
-			}
+//		System.out.println(projects.length);
+//		get the mockito and easy mock api
+		try {
+			GetMockitoEasyMock_API(projects);
+		} catch (CoreException e) {
 		}
-
-		print_to_csv(projects[0].getName());
 
 		return null;
 	}
 
-	private void analyseMethods(IProject project) throws JavaModelException {
-		IPackageFragment[] packages = JavaCore.create(project).getPackageFragments();
-		// parse(JavaCore.create(project));
-		for (IPackageFragment mypackage : packages) {
-			if (mypackage.getKind() == IPackageFragmentRoot.K_SOURCE) {
-
-				for (ICompilationUnit unit : mypackage.getCompilationUnits()) {// this is file level
-
-					// now create the AST for the ICompilationUnits
-					CompilationUnit parse = parse(unit);
-					MethodVisitor visitor = new MethodVisitor();
-					
-					try {
-						parse.accept(visitor);
-						for (MethodInvocation Mockito_method : visitor.getMockitoMethodInvocations()) {
-							mockito_arr.add(unit.getPath().toString() + "," + Mockito_method.getName() + '\n');
-						}
-
-						for (MethodInvocation EasyMock_method : visitor.getEasyMockMethodInvocations()) {
-							easymock_arr.add(unit.getPath().toString() + "," + EasyMock_method.getName() + '\n');
-						}
-					} catch (Exception e) {
-						err_arr.add(unit.getPath().toString() + '\n');
-					}
-
-
-				}
-
+	private boolean Import_mock(ICompilationUnit unit) throws CoreException {
+		if (unit.getImports().length <= 0) {
+			return false;
+		}
+		for (IImportDeclaration import_mock : unit.getImports()) {
+			if (import_mock.getElementName().contains("mockito") || import_mock.getElementName().contains("easymock")) {
+				return true;
 			}
 		}
+		return false;
+	}
+
+	private void GetMockitoEasyMock_API(IProject[] projects) throws CoreException {
+
+		ArrayList<String> mockito_arr = new ArrayList<>();
+		ArrayList<String> easymock_arr = new ArrayList<>();
+		ArrayList<String> have_mock = new ArrayList<>();
+
+		ArrayList<String> err_arr = new ArrayList<>();
+
+//		go throw all the project
+		for (IProject project : projects) {
+
+			if (project.isNatureEnabled(JDT_NATURE)) {
+
+				IPackageFragment[] packages = JavaCore.create(project).getPackageFragments();
+				for (IPackageFragment mypackage : packages) {
+					if (mypackage.getKind() == IPackageFragmentRoot.K_SOURCE) {
+
+						for (ICompilationUnit unit : mypackage.getCompilationUnits()) {// this is file level
+
+							// now create the AST for the ICompilationUnits
+							CompilationUnit parse = parse(unit);
+							if (Import_mock(unit)) {
+								try {
+									MethodVisitor visitor = new MethodVisitor();
+									parse.accept(visitor);
+									System.out.println(unit.getPath().toString());
+									for (MethodInvocation Mockito_method : visitor.getMockitoMethodInvocations()) {
+										mockito_arr
+												.add(unit.getPath().toString() + "," + Mockito_method.getName() + '\n');
+									}
+
+									for (MethodInvocation EasyMock_method : visitor.getEasyMockMethodInvocations()) {
+										easymock_arr.add(
+												unit.getPath().toString() + "," + EasyMock_method.getName() + '\n');
+									}
+								} catch (NullPointerException e) {
+									System.err.println(unit.getPath().toString());
+									err_arr.add(unit.getPath().toString() + '\n');
+								}
+							} 
+//							else {
+//								try {
+//									RQ4_visitor visitor = new RQ4_visitor();
+//									parse.accept(visitor);
+//									for (String Mock_method : visitor.getContainMock()) {
+//										have_mock.add(unit.getPath().toString() + "," + Mock_method + '\n');
+//									}
+//								} catch (Exception e) {
+//								}
+//							}
+
+						}
+
+					}
+				}
+			}
+		}
+//		print_to_csv(projects[0].getName());
+
+		String mockito_out = "new_RQ2\\mockito\\" + projects[0].getName() + ".csv";
+		String easymock_out = "new_RQ2\\easymock\\" + projects[0].getName() + ".csv";
+		String err = "new_RQ2\\err\\" + projects[0].getName() + ".csv";
+		String RQ4_out = "RQ4\\" + projects[0].getName() + ".csv";
+		System.out.println("Start writing");
+		print_arr_to_csv(mockito_arr, mockito_out);
+
+		print_arr_to_csv(easymock_arr, easymock_out);
+
+		print_arr_to_csv(have_mock, RQ4_out);
+
+		print_arr_to_csv(err_arr, err);
 
 	}
 
-	private void print_to_csv(String project_name) {
-		String mockito_out = "new_RQ2\\mockito\\" + project_name + ".csv";
-		String easymock_out = "new_RQ2\\easymock\\" + project_name + ".csv";
-		String err = "new_RQ2\\err\\" + project_name + ".csv";
-
-		// check weather have mockito api
-		if (mockito_arr.size() > 0) {
-			try (FileOutputStream fos = new FileOutputStream(mockito_out)) {
+	private void print_arr_to_csv(ArrayList<String> data, String path) {
+		if (data.size() > 0) {
+			try (FileOutputStream fos = new FileOutputStream(path)) {
 				fos.write("file_path,method\n".getBytes());
-				for (String x : mockito_arr) {
+				for (String x : data) {
 					fos.write(x.getBytes());
 				}
 
 				// Flush the written bytes to the file
 				fos.flush();
 
-				System.out.println("Text has  been  written to " + (new File(mockito_out)).getAbsolutePath());
+				System.out.println("Text has  been  written to " + (new File(path)).getAbsolutePath()+'\t'+data.size());
+
 			} catch (Exception e2) {
 				e2.printStackTrace();
-			}
-		} else {
-			System.out.println("no mockito api");
-		}
-		// check weather have easymock api
-		if (easymock_arr.size() > 0) {
-			try (FileOutputStream fos = new FileOutputStream(easymock_out)) {
-				fos.write("file_path,method\n".getBytes());
-				for (String x : easymock_arr) {
-					fos.write(x.getBytes());
-				}
-				fos.flush();
 
-				System.out.println("Text has  been  written to " + (new File(easymock_out)).getAbsolutePath());
-			} catch (Exception e2) {
-				e2.printStackTrace();
 			}
-		} else {
-			System.out.println("no easymock api");
 		}
 
-		if (err_arr.size() > 0) {
-			try (FileOutputStream fos = new FileOutputStream(err)) {
-				fos.write("file_path\n".getBytes());
-				for (String x : err_arr) {
-					fos.write(x.getBytes());
-				}
-				fos.flush();
-
-				System.out.println("err Text has  been  written to " + (new File(err)).getAbsolutePath());
-			} catch (Exception e2) {
-				e2.printStackTrace();
-			}
-		} else {
-			System.out.println("no err file");
-		}
 	}
 
 	private static CompilationUnit parse(ICompilationUnit unit) {
--- src/astsimple/handlers/MethodVisitor.java
+++ src/astsimple/handlers/MethodVisitor.java
@@ -16,50 +16,58 @@ public class MethodVisitor extends ASTVisitor {
 	List<MethodInvocation> mockito_methods = new ArrayList<>();
 	List<MethodInvocation> easymock_methods = new ArrayList<>();
 
+
 	List<MethodInvocation> methodInvocation = new ArrayList<>();
 
-	private TypeDeclaration parentTypeDeclaration;
-	private MethodDeclaration parentMethodDeclaration;
-
-	private final Map<MethodInvocation, ASTNode> methodInvocations = Map.of();
-
-	public void endVisit(TypeDeclaration typeDeclaration) {
-		this.parentTypeDeclaration = null;
-	}
-
-	public void endVisit(MethodDeclaration methodDeclaration) {
-		this.parentMethodDeclaration = null;
-	}
-
-	public boolean visit(TypeDeclaration typeDeclaration) {
-		this.parentTypeDeclaration = typeDeclaration;
-		return true;
-	}
-
-	public boolean visit(MethodDeclaration methodDeclaration) {
-		this.parentMethodDeclaration = parentMethodDeclaration;
-		return true;
-	}
+//
+//	private TypeDeclaration parentTypeDeclaration;
+//	private MethodDeclaration parentMethodDeclaration;
+//
+//	private final Map<MethodInvocation, ASTNode> methodInvocations = Map.of();
+//
+//	public void endVisit(TypeDeclaration typeDeclaration) {
+//		this.parentTypeDeclaration = null;
+//	}
+//
+//	public void endVisit(MethodDeclaration methodDeclaration) {
+//		this.parentMethodDeclaration = null;
+//	}
+//
+//	public boolean visit(TypeDeclaration typeDeclaration) {
+//		this.parentTypeDeclaration = typeDeclaration;
+//		return true;
+//	}
+//
+//	public boolean visit(MethodDeclaration methodDeclaration) {
+//		this.parentMethodDeclaration = parentMethodDeclaration;
+//		return true;
+//	}
 
 	@Override
 	public boolean visit(MethodInvocation node) {
 
-		ASTNode parentNode = Optional.ofNullable((ASTNode) parentMethodDeclaration).orElse(parentTypeDeclaration);
+//		ASTNode parentNode = Optional.ofNullable((ASTNode) parentMethodDeclaration).orElse(parentTypeDeclaration);
 
 		methodInvocation.add(node);
-//		if ((node.resolveMethodBinding() != null)&&(node.resolveMethodBinding().getDeclaringClass()!= null)) {//sometimes show error with resolveMethodBinding() is null, sometimes is .getDeclaringClass()
+		if ((node.resolveMethodBinding() != null) && (node.resolveMethodBinding().getDeclaringClass() != null)) {
+			// sometimes show error with resolveMethodBinding() is null, sometimes is
+			// .getDeclaringClass()
 
-		if (node.resolveMethodBinding().getDeclaringClass().getQualifiedName().startsWith("org.mockito")) {
-			mockito_methods.add(node);
-			methodInvocations.put(node, parentNode);
+			if (node.resolveMethodBinding().getDeclaringClass().getQualifiedName().startsWith("org.mockito")) {
+				mockito_methods.add(node);
+//			methodInvocations.put(node, parentNode);
+			}
+			if (node.resolveMethodBinding().getDeclaringClass().getQualifiedName().startsWith("org.easymock")) {
+				easymock_methods.add(node);
+			}
+		} else {
+			throw new NullPointerException();
 		}
-		if (node.resolveMethodBinding().getDeclaringClass().getQualifiedName().startsWith("org.easymock")) {
-			easymock_methods.add(node);
-		}
-//		}
 
 		return true;
 	}
+
+
 
 	public List<MethodInvocation> getAllMethodInvocations() {
 		return methodInvocation;
